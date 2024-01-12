60851b1388d63ab99fba937faafcb61b45b989ca
AlphaBolt <2028014@kiit.ac.in>
2024-01-12 22:17:41 +0530

    Sound Notifications
    Notification sound added. Also blank text messages cannot be send - added condition for that.

--- js/client.js
+++ js/client.js
@@ -8,11 +8,14 @@ const messageContainer = document.querySelector(".container");
 const name = prompt("Enter your name to join chat:");
 socket.emit('new-user-joined', name);  //This will emit an event which will be recieved by index.js of nodeServer
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();     // this will prevent the page from reloading when we press send
+form.addEventListener('submit', (event) => {
+    event.preventDefault();     // this will prevent the page from reloading when we press send
     const message = messageInput.value;
-    append(`You: ${message}`, 'right');
-    socket.emit('send', message);
+    
+    if(message.trim() != ""){         //this condition will not allow blank spaces to be send as text messages
+        append(`You: ${message}`, 'right');
+        socket.emit('send', message);
+    }
     messageInput.value = '';
 });
 
@@ -23,6 +26,11 @@ const append = (message, position) => {
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
+    // Playing audio:
+    if(position == 'left'){
+        var audio = new Audio("sounds/sound 1.mp3");
+        audio.play();
+    }
 };
 
 socket.on('user-joined', name => {
